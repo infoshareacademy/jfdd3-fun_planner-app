6807467c3244b85246736faa951530bd47ccc75f
Robert Dawidowski <dawidowski.robert@gmail.com>
2016-06-10 17:04:58 +0200

    szkielet kalendarz

--- aplication/javascript/visualization od calendar.js
+++ aplication/javascript/visualization od calendar.js
@@ -1,3 +1,16 @@
-/**
- * Created by mateuszszymula on 10.06.16.
- */
+"use strict";
+
+var $buttonLeft = $('#navi-left');
+var $buttonRight = $('#navi-right');
+
+$buttonLeft.click(function() {
+    var $addTopColumn = $('<div class= "calendar-column">');
+
+    $('#week-columns').prepend($addTopColumn);
+});
+
+$buttonRight.click(function() {
+    var $addTopColumn = $('<div class= "calendar-column">');
+
+    $('#week-columns .navigation.right').before($addTopColumn);
+});

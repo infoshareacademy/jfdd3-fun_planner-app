76ba22ea13c70ac40119d09de01e048eb87e8f1c
Mateusz Szymula <mszymula@wp.pl>
2016-06-17 12:04:01 +0200

    zamiana mapy js na angular js

--- aplication/javascript/app.js
+++ aplication/javascript/app.js
@@ -3,12 +3,17 @@
  */
 
 (function () {
-    var app = angular.module('FunPlanner', []);
+    var app = angular.module('FunPlanner', ['uiGmapgoogle-maps']);
 
     app.controller('FunPlannerController', ctrl);
+    app.controller('uiGmapgoogle-mapsController', mapCtrl);
 
     function ctrl($scope) {
         $scope.name = 'Mateusz'
+    }
+
+    function mapCtrl($scope) {
+        $scope.map = {center: {latitude: 54.4444, longitude: 18.56444}, zoom: 10, disableDefaultUI: true, zoomControl: false, scaleControl: true}
     }
 
 })();

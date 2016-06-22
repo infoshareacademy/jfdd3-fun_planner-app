892f983fa5aa75e5065ef21c05e29cab0fe8b950
Robert Dawidowski <dawidowski.robert@gmail.com>
2016-06-22 09:16:43 +0200

    logowanie google+

--- aplication/javascript/aplication.js
+++ aplication/javascript/aplication.js
@@ -461,3 +461,19 @@ function deleteEvent(value) {
     $('.list-element').popover('hide');
     value.parent().parent().remove();
 }
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    console.log('ID: ' + profile.getId());
+    console.log('Name: ' + profile.getName());
+    console.log('Image URL: ' + profile.getImageUrl());
+    console.log('Email: ' + profile.getEmail());
+}

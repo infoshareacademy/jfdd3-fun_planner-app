8374952038601fac235c028043a8add3b4a9893b
Robert Dawidowski <dawidowski.robert@gmail.com>
2016-06-22 15:53:13 +0200

    logowanie google+ poprawione buttony

--- aplication/javascript/app.js
+++ aplication/javascript/app.js
@@ -1,35 +1,38 @@
-/**
- * Created by mateuszszymula on 17.06.16.
- */
 
 (function () {
-    var app = angular.module('FunPlanner', ['uiGmapgoogle-maps', 'angular-loading-bar']);
+    var app = angular.module('FunPlanner', ['angular-loading-bar']);
 
-    app.controller('FunPlannerController', ctrl);
-    app.controller('uiGmapgoogle-mapsController', mapCtrl);
-    app.controller('loadingBar', loadingBarCtrl);
+    app.controller('FunPlannerController', function ($scope){
 
+        $scope.signedIn = false;
+        $scope.signOut = signOut;
 
-    function ctrl ($scope) {
-
-
-
-    }
-
-    function mapCtrl($scope) {
-        $scope.map = {center: {
-            latitude: 54.4444,
-            longitude: 18.56444},
-            zoom: 10,
-            disableDefaultUI: true,
-            zoomControl: false,
-            scaleControl: true
+        function onSignIn(googleUser) {
+            var profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId());
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail());
+            $scope.signedIn = true;
+            $scope.$apply();
         }
-    }
 
-    function loadingBarCtrl() {
+        window.onSignIn = onSignIn;
 
+        function signOut() {
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+                console.log('User signed out.');
+                $scope.signedIn = false;
+                $scope.$apply();
+                setTimeout(function () {
+                    $scope.$apply()
+                }, 1000)
+            });
+            $scope.$apply();
+        }
 
-    }
+        window.signOut = signOut;
 
+    });
 })();

724662485063f712190d4ff3ec920f39e0768ae3
Mateusz Szymula <mszymula@wp.pl>
2016-06-24 14:04:08 +0200

    nowy button zamknięcie mapy,

--- aplication/javascript/map.js
+++ aplication/javascript/map.js
@@ -7,9 +7,13 @@ $(document).ready(function () {
     $ButtonMap.on('click', function () {
         $('.calendar-position').hide();
         $('.pull-right').hide();
+        $('#button-map').hide();
+        $('#hide-button-map').show();
+        $('.hide-button-map').css({"display": "inline"});
         $('.angular-google-map-container').css({"left": "35%"});
 
     });
+
 
 
 

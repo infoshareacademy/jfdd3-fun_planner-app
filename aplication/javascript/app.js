0c08ba4263ffd0d524326243d9fd3957be2737bf
Mateusz Szymula <mszymula@wp.pl>
2016-06-27 11:05:19 +0200

    mapa działa, jeszcze markery nie

--- aplication/javascript/app.js
+++ aplication/javascript/app.js
@@ -5,6 +5,55 @@
 (function () {
     var app = angular.module('FunPlanner', ['angular-loading-bar']);
     app.controller('loadingBar', loadingBarCtrl);
+    app.controller('MapCtrl', function ($scope) {
+
+        var mapOptions = {
+            zoom: 11,
+            center: new google.maps.LatLng(54.432448, 18.594874),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+
+        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+        $scope.markers = [];
+
+        var infoWindow = new google.maps.InfoWindow();
+
+        var createMarker = function (x){
+
+            var image = 'images/publicart.png';
+            var marker = new google.maps.Marker({
+                map: $scope.map,
+                position: new google.maps.LatLng(x.latitude, x.longitude),
+                title: x.name,
+                icon: image
+            });
+            marker.content = '<div class="infoWindowContent">' + '<h>x.eventName</h>' + '<p>x.info</p>' + '</div>';
+
+
+            google.maps.event.addListener(marker, 'click', function(){
+                $scope.openInfoWindow(null, marker);
+                $scope.$digest();
+            });
+
+            $scope.markers.push(marker);
+
+        }
+
+        for (i = 0; i < cityEvents.length; i++){
+            createMarker(cityEvents[i]);
+        }
+
+        $scope.openInfoWindow = function(e, selectedMarker){
+            e && e.preventDefault();
+
+            infoWindow.setContent('<h2>' + selectedMarker.title + '</h2>' + selectedMarker.content);
+            infoWindow.open($scope.map, selectedMarker);
+            $scope.markerId = $scope.markers.indexOf(selectedMarker);
+
+        }
+    });
+
     app.controller('FunPlannerController', function ($scope) {
 
         $scope.signedIn = false;
@@ -39,28 +88,6 @@
 
     });
 
-    // function mapCtrl($scope) {
-    //     $scope.map = {center: {
-    //         latitude: 54.432448,
-    //         longitude: 18.594874},
-    //         zoom: 11,
-    //         disableDefaultUI: true,
-    //         zoomControl: false,
-    //         scaleControl: true,
-    //         bounds: {}
-    //     };
-    //     $scope.marker = {
-    //         id: 0,
-    //         coords: {
-    //             latitude: 54.443647,
-    //             longitude: 18.565829
-    //         },
-    //         options: { draggable: true },
-    //         icon: {
-    //             url: 'images/publicart.png',
-    //         },
-    //     };
-    // };
 
 
     function loadingBarCtrl () {
@@ -69,51 +96,3 @@
 })();
 
 
-angular.module('mapsApp', []).controller('MapCtrl', function ($scope) {
-
-    var mapOptions = {
-        zoom: 11,
-        center: new google.maps.LatLng(54.432448, 18.594874),
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    }
-
-        $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-        $scope.markers = [];
-
-        var infoWindow = new google.maps.InfoWindow();
-
-        var createMarker = function (x){
-
-            var image = 'images/publicart.png'
-            var marker = new google.maps.Marker({
-                map: $scope.map,
-                position: new google.maps.LatLng(x.latitude, x.longitude),
-                title: x.name,
-                icon: image
-            });
-            marker.content = '<div class="infoWindowContent">' + '<h>x.eventName</h>' + '<p>x.info</p>' + '</div>';
-
-
-            google.maps.event.addListener(marker, 'click', function(){
-                $scope.openInfoWindow(null, marker);
-                $scope.$digest();
-            });
-
-            $scope.markers.push(marker);
-
-        }
-
-        for (i = 0; i < cityEvents.length; i++){
-            createMarker(cityEvents[i]);
-        }
-
-        $scope.openInfoWindow = function(e, selectedMarker){
-            e && e.preventDefault();
-
-            infoWindow.setContent('<h2>' + selectedMarker.title + '</h2>' + selectedMarker.content);
-            infoWindow.open($scope.map, selectedMarker);
-            $scope.markerId = $scope.markers.indexOf(selectedMarker);
-
-        }
-    });
